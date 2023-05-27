e24bc504d25dcd3b1e1b8d288bb7f16d8d9a32a1
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-05-27 15:12:29 -0300

    padronizando nomenclatura

--- src/main/java/com/ecommerce/model/Cliente.java
+++ src/main/java/com/ecommerce/model/Cliente.java
@@ -1,4 +1,4 @@
-package com.ecommerce.model;
+package com.ecommerce.Model;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/com/ecommerce/model/Pedido.java
+++ src/main/java/com/ecommerce/model/Pedido.java
@@ -1,4 +1,4 @@
-package com.ecommerce.model;
+package com.ecommerce.Model;
 
 import java.util.List;
 
