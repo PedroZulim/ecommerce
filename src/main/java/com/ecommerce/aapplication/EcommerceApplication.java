e510f258aa8e80af7c4bcd8691f49e3eb80eeab1
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-05-27 15:46:33 -0300

    Convenção para nomes

--- src/main/java/com/ecommerce/aapplication/EcommerceApplication.java
+++ src/main/java/com/ecommerce/aapplication/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.application;
+package com.ecommerce.aapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
