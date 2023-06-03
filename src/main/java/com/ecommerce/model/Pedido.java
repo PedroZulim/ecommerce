d99701508365d82dddc4b7c99c62dd27a9de48d1
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-06-03 11:48:56 -0300

    Alterando Model

--- src/main/java/com/ecommerce/model/Pedido.java
+++ src/main/java/com/ecommerce/model/Pedido.java
@@ -1,14 +1,22 @@
 package com.ecommerce.model;
 
 import java.util.List;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Cliente cliente;
     private List<ItemPedido> itens;
+    private double valor;
 }
