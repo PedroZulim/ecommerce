7ca7a4ccfcfc7bc645abb850dd3c78eeaa4b3957
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-06-04 22:55:26 -0300

    Reestruturando ClienteService

--- src/main/java/com/ecommerce/service/ClienteService.java
+++ src/main/java/com/ecommerce/service/ClienteService.java
@@ -1,32 +1,45 @@
 package com.ecommerce.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.ecommerce.model.Cliente;
-import com.ecommerce.repository.ClienteRepository;
 
-@Service
+@RestController
 public class ClienteService {
-  
-  @Autowired
-  private ClienteRepository clienteRepository;
+	
+  RestTemplate restTemplate;
 
-  public List<Cliente> getAllClientes() {
-    return clienteRepository.findAll();
-  }
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 
-  public Cliente getClienteById(Long id) {
-    return clienteRepository.findById(id).orElse(null);
-  }
+	@RequestMapping(path="/getcliente", method=RequestMethod.GET)
+	public ResponseEntity<String> getCliente(Long id){
 
-  public Cliente saveCliente(Cliente cliente) {
-    return clienteRepository.save(cliente);
-  }
+		Cliente respCli = new Cliente();
+		String errResp = "";
+		ResponseEntity<String> response = null;
 
-  public void deleteCliente(Long id) {
-    clienteRepository.deleteById(id);
-  }
+		try {
+
+			response = new ResponseEntity<String>(respCli.toString(), HttpStatus.OK);
+		
+		} catch (Exception e) {
+
+			errResp = "{\"sucesso\": \"False - Erro ao localizar cliente !\"}";
+			response = new ResponseEntity<String>(errResp, HttpStatus.EXPECTATION_FAILED);
+
+		}
+		return response;
+		
+	}
+	
 }
