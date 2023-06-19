ae983641a38cc82134477dd154fd88d3647cc7e6
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-06-19 12:00:37 -0300

    .

--- src/main/java/com/ecommerce/service/ClienteService.java
+++ src/main/java/com/ecommerce/service/ClienteService.java
@@ -3,13 +3,21 @@ package com.ecommerce.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.ecommerce.model.Cliente;
 import com.ecommerce.repository.ClienteRepository;
 
 @Service
 public class ClienteService {
+
+  @Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
   
   @Autowired
   private ClienteRepository clienteRepository;
