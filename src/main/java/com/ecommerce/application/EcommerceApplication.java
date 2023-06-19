c8448f6b82495a807559c7a12af2ff3832f4efd0
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-06-19 15:35:26 -0300

    Aplication

--- src/main/java/com/ecommerce/application/EcommerceApplication.java
+++ src/main/java/com/ecommerce/application/EcommerceApplication.java
@@ -2,24 +2,11 @@ package com.ecommerce.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.ecommerce.model.Cliente;
-import com.ecommerce.model.Produto;
-import com.ecommerce.repository.ClienteRepository;
-import com.ecommerce.repository.ProdutoRepository;
-import com.ecommerce.service.ClienteService;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(EcommerceApplication.class, args);
-
-		ClienteService clienteService = context.getBean(ClienteService.class);
-
-		Cliente cliente = new Cliente();
-		System.out.println(clienteService.getAllClientes());
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
 }
